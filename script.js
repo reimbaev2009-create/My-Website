abf7463ebfd5ba9c1bb404beee958e10de345228
Beybars <reimbaev2009@gmail.com>
2026-08-18 19:11:54 +0300

    Добавил базовую разметку сайта

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('BeybarsBot Loaded');
+// Функция переключения заглавия в шапке
+function switchPage(pageName) {
+  const btnHome = document.getElementById('btnNavHome');
+  const btnSignals = document.getElementById('btnNavSignals');
+  const pageTitle = document.getElementById('pageTitle');
+
+  if (pageName === 'home') {
+    btnHome.classList.add('active');
+    btnSignals.classList.remove('active');
+    pageTitle.textContent = 'Главное';
+  } else if (pageName === 'signals') {
+    btnSignals.classList.add('active');
+    btnHome.classList.remove('active');
+    pageTitle.textContent = 'Сигналы';
+  }
+}
